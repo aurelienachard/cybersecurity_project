a589e3775193ad9d7bffa399955796277fdabe26
aurelienachard <aurelien.achard@proton.me>
2025-03-15 17:27:18 +0100

    feat: ajout boutons de deconnexion

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,11 +6,16 @@ import Tool from './pages/tool'
 import './style.css'
 
 const App = () => {
+  const isAuthenticated = () => {
+    const token = localStorage.getItem('token')
+    return token && token.length > 0
+  }
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />}/>
-        <Route path="/tool" element={<Tool />}/>
+        <Route path="/tool" element={isAuthenticated() ? <Tool /> : <Navigate to="/" />}/>
       </Routes>
     </Router>
   )
