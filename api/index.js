ceab0f757c1a2a4405eebf39a9300023efd1bc25
aurelienachard <aurelien.achard@proton.me>
2025-03-15 15:35:38 +0100

    feat: ajout de la route connexion et inscription

--- api/index.js
+++ api/index.js
@@ -4,9 +4,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mysql = require('mysql2')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 app.use(express.json())
 app.use(cors())
+
+const secretKey = process.env.SECRET_KEY
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -21,6 +25,66 @@ db.connect((error) => {
     } else {
         console.log('connected')
     }
+})
+
+// route de connexion
+
+app.post('/authentication', (request, response) => {
+    const {username, password} = request.body
+
+    db.query('select * from users where username = ?', username, (error, result) => {
+        if (error) {
+            console.log(error)
+            return response.json(error)
+        }
+
+        if (result.length === 0) {
+            return response.status(401).json({message: 'username introuvable'})
+        }
+
+        const username = result[0]
+
+        bcrypt.compare(password, username.password, (error, match) => {
+            if (error) {
+                console.log(error)
+                return response.json(error)
+            }
+
+            if (!match) {
+                return response.status(401).json({message: 'mot de passe incorrect'})
+            }
+
+            const token = jwt.sign({id: username.user_id}, secretKey, {expiresIn: '1h'})
+            return response.json({ token })
+        })
+    })
+})
+
+// creation d'un utilisateur
+
+app.post('/register', (request, response) => {
+    const {username, password} = request.body
+    const saltRound = 12
+
+    bcrypt.genSalt(saltRound, (error, salt) => {
+        if (error) {
+            console.log(error)
+        }
+
+        bcrypt.hash(password, salt, (error, hash) => {
+            if (error) {
+                console.log(error)
+            }
+
+            db.query('insert into users (username, password) values (?, ?)', [username, hash], (error) => {
+                if (error) {
+                    console.log(error)
+                } else {
+                    response.json('values inserted')
+                }
+            })
+        })
+    })
 })
 
 const PORT = process.env.PORT
