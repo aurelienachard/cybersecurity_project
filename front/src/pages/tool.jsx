0f18ad4d7c6deb71c31ba86c6cdc629426fc21b1
aurelienachard <aurelien.achard@proton.me>
2025-03-15 19:01:45 +0100

    feat: petit design du header

--- front/src/pages/tool.jsx
+++ front/src/pages/tool.jsx
@@ -20,8 +20,14 @@ const Tool = () => {
 
     return (
         <div className="bg-gray-900 min-h-screen w-full">
-            <h1 className="text-white">You're connected</h1>
-            <button onClick={handleDeconexion} className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Deconnexion</button>
+            <header className="flex items-center justify-between p-[24px] bg-gray-950">
+                <h1 className="text-white font-bold text-[24px]">CYBER</h1>
+                <button
+                    onClick={handleDeconexion}
+                    className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                    Deconnexion
+                </button>
+            </header>
         </div>
     )
 }
