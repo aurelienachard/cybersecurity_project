01d0fc3a63c9491c28c9c49ce7ae4b7cc06b6e01
aurelienachard <aurelien.achard@proton.me>
2025-03-16 11:48:46 +0100

    feat: petit ajout de texte

--- front/src/pages/home.jsx
+++ front/src/pages/home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
 
                 <div className="rounded-lg bg-gray-800 shadow-sm m-[24px] w-[500px]">
                     <div className="px-4 py-5 sm:p-6">
-                        <h2 className="text-[18px] font-bold text-gray-200">Trouver les failles de sécurité</h2>
+                        <h2 className="text-[18px] font-bold text-gray-200">Trouver les failles de sécurité (Basique)</h2>
                         
                         <p className="text-[16px] text-gray-400 mt-[10px] mb-[10px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Duis sit amet aliquam purus. Maecenas faucibus elit lectus.
